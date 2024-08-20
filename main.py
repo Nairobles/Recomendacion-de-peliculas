1133bbe25d1a8f070c7d40c9188ae895d5a8a883
Nairobles <155001844+Nairobles@users.noreply.github.com>
2024-08-20 15:40:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -59,6 +59,6 @@ async def recomendacion(titulo:str):
     return respuesta
 
 #Cambia el puerto para que corra en Render
-if _name_ == "_main_":
+if __name__== "__main__":
     port = int(os.environ.get("PORT", 10000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
